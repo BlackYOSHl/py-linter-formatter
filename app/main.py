53dfcf661ef55402630a1e114485bd6c45785095
Yurii Lesyk <lesik.yurko@gmail.com>
2024-11-20 22:07:10 +0200

    Fix code style issues (E271, E501, W391)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def format_linter_error(error: dict) -> dict:
-    return  {
+    return {
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
@@ -17,5 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
-
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
